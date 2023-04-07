b95b3bf0503bf11824b1843f210d25b93926e596
ideasound <wosk0106@argonet.co.kr>
2023-04-07 17:41:21 +0900

    initial commit

---
+++ tsconfig.js
@@ -0,0 +1,12 @@
+{
+  "compilerOptions": {
+    "target": "es5",
+    "module": "commonjs",
+    "strict": true,
+    "esModuleInterop": true,
+    "outDir": "js"
+  },
+  "include": ["ts/**/*.ts"],
+  "exclude": ["node_modules"]
+}
+
